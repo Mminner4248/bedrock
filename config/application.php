6d5ec4aadb05bd78aa5a7a457d519f99a3922f79
John Ouellet <labboy0276@yahoo.com>
2017-12-01 19:02:55 -0500

    Update to skip hash on lando env

--- config/application.php
+++ config/application.php
@@ -85,15 +85,19 @@ else {
 
   /**
    * Authentication Unique Keys and Salts
+   *
+   * Skip for Lando.
    */
-  define('AUTH_KEY', env('AUTH_KEY'));
-  define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
-  define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
-  define('NONCE_KEY', env('NONCE_KEY'));
-  define('AUTH_SALT', env('AUTH_SALT'));
-  define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
-  define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
-  define('NONCE_SALT', env('NONCE_SALT'));
+  if (!env('LANDO_APP')) {
+    define('AUTH_KEY', env('AUTH_KEY'));
+    define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+    define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+    define('NONCE_KEY', env('NONCE_KEY'));
+    define('AUTH_SALT', env('AUTH_SALT'));
+    define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+    define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+    define('NONCE_SALT', env('NONCE_SALT'));
+  }
 
   /**
    * Custom Settings
